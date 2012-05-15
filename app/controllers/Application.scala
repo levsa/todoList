a3021a1d5f66b5e233dcc52307093355b7ad5f69
Levon Saldamli <levon.sa@gmail.com>
2012-05-15 19:05:15 +0200

    Added routes for tasks

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,15 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("Hello Världen")
   }
-  
+
+  def tasks = TODO
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
+
 }
